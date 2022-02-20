5874287e9364fb99f9168f8ea446dd1ca10b40a6
m00thie <5d4a7ioo@dohndoe.anonaddy.com>
2022-02-20 16:55:09 +0100

    init project setup

---
+++ src/main/java/com/exercise/system/exercisesystem/ExerciseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.system.exercisesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/system/exercisesystem/ExerciseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.system.exercisesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
