3e265313730d1128699cf519d98a3637248eefb5
Andrzej Rawski <rawski.andrzej@gmail.com>
2022-02-27 19:39:23 +0100

    Spring Data JPA Services created

---
+++ src/main/java/com/exercise/system/exercisesystem/services/RoomService.java
@@ -0,0 +1,8 @@
+package com.exercise.system.exercisesystem.services;
+
+import com.exercise.system.exercisesystem.model.Room;
+
+import java.util.UUID;
+
+public interface RoomService extends CrudService<Room, UUID> {
+}
